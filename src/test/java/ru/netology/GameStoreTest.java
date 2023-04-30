2cc815d2a249f89a2457290bfdc626c275ad5a0f
Ksenia Vi <kseniavi.ly@gmail.com>
2023-04-30 13:51:12 +0300

    add new tests in the gamestore

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -16,6 +16,16 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game));
     }
 
+    @Test //Добавление двух игр и поиск в каталоге
+    public void shouldAddTwoGame() {
+
+        Game game1 = store.publishGame("Шахматы", "Логические");
+        Game game2 = store.publishGame("Тетрис", "Логические");
+
+        assertTrue(store.containsGame(game1));
+        assertTrue(store.containsGame(game2));
+    }
+
 
     @Test //Проверяет наличие недобавленной игры
     public void searchNonExistentGame() {
@@ -25,19 +35,19 @@ public class GameStoreTest {
         assertFalse(store.containsGame(game));
     }
 
-//    @Test //регистрация и суммирование времени игрока
-//    public void addPlayTime() {
-//
-//        store.addPlayTime("Player1", 3);
-//        store.addPlayTime("Player1", 4);
-//        store.addPlayTime("Player1", 5);
-//
-//        int expected = 12;
-//        int actual =  //Вот тут нужно, чтобы суммировалось время одного игрока
-//
-//        Assertions.assertEquals(expected, actual);
-//
-//    }
+    @Test //регистрация и суммирование времени одного игрока
+    public void addPlayTime() {
+
+        store.addPlayTime("Player1", 3);
+        store.addPlayTime("Player1", 4);
+        store.addPlayTime("Player1", 5);
+
+        int expected = 12;
+        int actual = store.fiendPlayedTime().get("Player1");
+
+        Assertions.assertEquals(expected, actual);
+
+    }
 
     @Test //поиск игрока, который сыграл дольше всех
     public void showBestPlayer() {
@@ -52,10 +62,55 @@ public class GameStoreTest {
 
     }
 
+
+    @Test //поиск игрока, который сыграл дольше всех, если игрок один
+    public void showBestPlayerOnePlayer() {
+        store.addPlayTime("Player1", 30);
+
+        String expected = "Player1";
+        String actual = store.getMostPlayer();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
     @Test //если игроков нет
     public void showBestPlayerNull() {
 
-        String expected = "null";
+        String expected = null;
+        String actual = store.getMostPlayer();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test //поиск игрока, если он один и сыграл 1 час
+    public void showBestPlayerOneHour() {
+        store.addPlayTime("Player1", 1);
+
+        String expected = "Player1";
+        String actual = store.getMostPlayer();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test //поиск игрока, если он один и сыграл 0 часов
+    public void showBestPlayerZeroHour() {
+        store.addPlayTime("Player1", 0);
+
+        String expected = "Player1";
+        String actual = store.getMostPlayer();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test //поиск игрока, если он один и сыграл 2 часа
+    public void showBestPlayer6() {
+        store.addPlayTime("Player1", 2);
+
+        String expected = "Player1";
         String actual = store.getMostPlayer();
 
         Assertions.assertEquals(expected, actual);
@@ -63,14 +118,49 @@ public class GameStoreTest {
     }
 
 
-    @Test //суммирование времени 3 игроков
-    public void sumPlayedTime() {
+    @Test //суммирование общего времени 3 игроков
+    public void sumPlayedTimeThreePlayers() {
 
         store.addPlayTime("Player1", 30);
         store.addPlayTime("Player2", 50);
         store.addPlayTime("Player3", 30);
 
         int expected = 110;
+        int actual = store.getSumPlayedTime();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test //суммирование общего времени 2 игроков
+    public void sumPlayedTimeTwoPlayers() {
+
+        store.addPlayTime("Player1", 30);
+        store.addPlayTime("Player2", 50);
+
+        int expected = 80;
+        int actual = store.getSumPlayedTime();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test //суммирование общего времени, если игрок один
+    public void sumPlayedTimeOnePlayer() {
+
+        store.addPlayTime("Player1", 30);
+
+        int expected = 30;
+        int actual = store.getSumPlayedTime();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test //суммирование общего времени, если игроков нет
+    public void sumPlayedTimeNoPlayers() {
+
+        int expected = 0;
         int actual = store.getSumPlayedTime();
 
         Assertions.assertEquals(expected, actual);
